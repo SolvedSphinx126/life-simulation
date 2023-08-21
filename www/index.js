b44a3014b4685214fd4f802219dff2b66f926cd7
Ben O'Bryan <obryanbw@gmail.com>
2023-08-21 10:38:24 -0500

    added basic web layout

--- www/index.js
+++ www/index.js
@@ -1,3 +1,17 @@
 import * as wasm from "life_simulation";
 
-wasm.greet();
+var canvas = document.getElementById("game-canvas");
+var ctx = canvas.getContext("2d");
+canvas.width = 500;
+canvas.height = 500;
+
+const drawBg = () => {
+    ctx.fillStyle = "#EEEEEE";
+    ctx.fillRect(0,0,canvas.width,canvas.height);
+}
+
+const render = () => {
+    drawBg();
+}
+
+render()
