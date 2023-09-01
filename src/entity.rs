0bff8e3230064f63cd88157cf60cbbb530e2b520
Ben O'Bryan <obryanbw@gmail.com>
2023-08-31 19:19:30 -0500

    fixed errors

--- src/entity.rs
+++ src/entity.rs
@@ -1,4 +1,5 @@
-trait Entity {
+pub mod Entity {
+pub trait Entity {
     fn get_id(&self) -> u32;
     fn get_x(&self) -> i32;
     fn get_y(&self) -> i32;
@@ -33,15 +34,15 @@ impl Rock{
 
 
 struct Plant{
-    id: u32;
-    x: i32;
-    y: i32;
-    state: i32; // might need more complexity
-    growth_rate: i32;
-    const MAX_SIZE: i32;
-    const MAX_SEED_NUMBER: i32;
-    const MAX_SEED_DISTANCE: i32;
-    const SEED_VIABILITY: i32; //should this be constant?
+    id: u32,
+    x: i32,
+    y: i32,
+    state: i32, // might need more complexity
+    growth_rate: f32,
+    max_size: i32,
+    max_seed_number: i32,
+    max_seed_distance: i32,
+    seed_viability: i32, //should this be constant?
     //plant height and diameter is the same
 }
 
@@ -61,8 +62,9 @@ impl Entity for Plant{
 }
 
 impl Plant{
-    fn get_growth_rate(&self) -> u32 {
+    fn get_growth_rate(&self) -> f32 {
         self.growth_rate
     }
 }
 
+}
