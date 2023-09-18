f4fb595f4d38168a715c0e7e013857b3301229b6
Ben O'Bryan <obryanbw@gmail.com>
2023-09-18 13:51:24 -0500

    add xml parsing into wasm structs

--- src/lib.rs
+++ src/lib.rs
@@ -8,41 +8,42 @@ extern "C" {
 //use crate::grazer::grazer::Grazer;
 //use crate::plant::plant::Plant;
 //use crate::rock::rock::Rock;
-use wasm_bindgen::{prelude::wasm_bindgen, JsValue};
 use js_sys::Array;
+use wasm_bindgen::{prelude::wasm_bindgen, JsValue};
 
 #[derive(Default)]
 #[wasm_bindgen]
 pub struct Map {
-    width: u32,
-    height: u32,
-    current_tick: u64,
+    pub width: u32,
+    pub height: u32,
+    pub current_tick: u64,
     //world constants
     //plant
-    init_plant_count: u32,
-    growth_rate: f32,
-    max_size: u32,
-    max_seed_cast_distance: u32,
-    max_seed_number: u32,
-    seed_viability: f32,
+    //counts arent needed
+    pub init_plant_count: u32,
+    pub growth_rate: f32,
+    pub max_size: u32,
+    pub max_seed_cast_distance: u32,
+    pub max_seed_number: u32,
+    pub seed_viability: f32,
     //grazer
-    init_grazer_count: u32,
-    grazer_energy_input: u32,
-    grazer_energy_output: u32,
-    grazer_energy_to_reproduce: u32,
-    grazer_maintain_speed: f32,
-    grazer_max_speed: f32,
+    pub init_grazer_count: u32,
+    pub grazer_energy_input: u32,
+    pub grazer_energy_output: u32,
+    pub grazer_energy_to_reproduce: u32,
+    pub grazer_maintain_speed: f32,
+    pub grazer_max_speed: f32,
     //predator
-    init_predator_count: u32,
-    max_speed_hod: f32,
-    max_speed_hed: f32,
-    max_speed_hor: f32,
-    predator_maintain_speed: f32,
-    predator_energy_output: u32,
-    predator_energy_to_reproduce: u32,
-    predator_max_offspring: u32,
-    predator_gestation: f32,
-    predator_offspring_energy: u32,
+    pub init_predator_count: u32,
+    pub max_speed_hod: f32,
+    pub max_speed_hed: f32,
+    pub max_speed_hor: f32,
+    pub predator_maintain_speed: f32,
+    pub predator_energy_output: u32,
+    pub predator_energy_to_reproduce: u32,
+    pub predator_max_offspring: u32,
+    pub predator_gestation: f32,
+    pub predator_offspring_energy: u32,
 
     rocks: Vec<Rock>,
     predators: Vec<Predator>,
@@ -65,45 +66,58 @@ impl Map {
     pub fn get_height(&self) -> u32 {
         self.height
     }
-    
+
     pub fn get_rocks(&self) -> js_sys::Array {
-        self.rocks.clone().into_iter().map(JsValue::from).collect::<js_sys::Array>()
+        self.rocks
+            .clone()
+            .into_iter()
+            .map(JsValue::from)
+            .collect::<js_sys::Array>()
     }
     pub fn get_grazers(&self) -> js_sys::Array {
-        self.grazers.clone().into_iter().map(JsValue::from).collect::<js_sys::Array>()
+        self.grazers
+            .clone()
+            .into_iter()
+            .map(JsValue::from)
+            .collect::<js_sys::Array>()
     }
     pub fn get_plants(&self) -> js_sys::Array {
-        self.plants.clone().into_iter().map(JsValue::from).collect::<js_sys::Array>()
+        self.plants
+            .clone()
+            .into_iter()
+            .map(JsValue::from)
+            .collect::<js_sys::Array>()
     }
     pub fn get_predators(&self) -> js_sys::Array {
-        self.predators.clone().into_iter().map(JsValue::from).collect::<js_sys::Array>()
+        self.predators
+            .clone()
+            .into_iter()
+            .map(JsValue::from)
+            .collect::<js_sys::Array>()
     }
-    pub fn add_rock(&mut self, id: u32, x: i32, y: i32, diameter: u32, height: u32) -> Rock{
-        let new_rock = Rock::new(id, x, y, diameter, height);
+    pub fn add_rock(&mut self, x: i32, y: i32, diameter: u32, height: u32) {
+        let new_rock = Rock::new(x, y, diameter, height);
         self.rocks.push(new_rock);
-        new_rock
     }
-    pub fn add_plant(&mut self, id: u32, x: i32, y: i32, diameter: u32){
+    pub fn add_plant(&mut self, id: u32, x: i32, y: i32, diameter: u32) {
         let new_plant: Plant = Plant::new(id, x, y, diameter);
         self.plants.push(new_plant);
     }
-    pub fn add_grazer(&mut self,new_id: u32, new_x:i32, new_y: i32, new_state: i32, new_velocity_x: i32, new_velocity_y: i32, new_orientation: f32, new_target_x: i32,new_target_y: i32, new_energy: i32, new_min_in_loc: i32){
-        let new_grazer = Grazer::new(new_id, new_x, new_y, new_state, new_velocity_x, new_velocity_y, new_orientation, new_target_x, new_target_y, new_energy, new_min_in_loc);
+    pub fn add_grazer(&mut self, new_x: i32, new_y: i32, new_energy: i32) {
+        let new_grazer = Grazer::new(new_x, new_y, new_energy);
         self.grazers.push(new_grazer);
     }
-    pub fn add_predator(&mut self, new_id: u32, new_x:i32, new_y: i32, new_state: i32, new_velocity_x: i32, new_velocity_y: i32, new_orientation: f32, new_target_x: i32,new_target_y: i32, new_energy: i32, new_min_in_loc: i32, new_gen_seq: String, new_family: Vec<i32>, new_time_family: f32, new_is_pregnant: bool, new_time_til_birth: u64, new_mate_gen_seq: String){
-        let new_predator = Predator::new(new_id, new_x, new_y, new_state, new_velocity_x, new_velocity_y, new_orientation, new_target_x, new_target_y, new_energy, new_min_in_loc, new_gen_seq, new_family, new_time_family, new_is_pregnant, new_time_til_birth, new_mate_gen_seq);
+    pub fn add_predator(&mut self, new_x: i32, new_y: i32, new_energy: i32, new_gen_seq: String) {
+        let new_predator = Predator::new(new_x, new_y, new_energy, new_gen_seq);
         self.predators.push(new_predator)
     }
-    
+
     pub fn set_width(&mut self, new_width: u32) {
         self.width = new_width;
     }
     pub fn set_height(&mut self, new_height: u32) {
         self.height = new_height;
     }
-    
-
 
     //plants
     pub fn get_init_plant_count(&self) -> u32 {
@@ -142,7 +156,6 @@ impl Map {
     pub fn set_seed_viability(&mut self, new_seed_viability: f32) {
         self.seed_viability = new_seed_viability;
     }
-    
 
     //Grazers
     pub fn get_init_grazer_count(&self) -> u32 {
@@ -240,14 +253,17 @@ impl Map {
     pub fn set_predator_offspring_energy(&mut self, new_predator_offspring_energy: u32) {
         self.predator_offspring_energy = new_predator_offspring_energy;
     }
+    pub fn set_predator_max_offspring(&mut self, new_predator_max_offspring: u32) {
+        self.predator_max_offspring = new_predator_max_offspring;
+    }
 }
 
 #[derive(Clone, Copy)]
 #[wasm_bindgen]
 pub struct Entity {
-    id: u32,
-    x: i32,
-    y: i32,
+    pub id: u32,
+    pub x: i32,
+    pub y: i32,
 }
 
 #[wasm_bindgen]
@@ -259,7 +275,7 @@ impl Entity {
             y: new_y,
         }
     }
-    fn get_id(&self) ->u32 {
+    fn get_id(&self) -> u32 {
         self.id
     }
 
@@ -284,23 +300,40 @@ impl Entity {
     }
 }
 
+impl Default for Entity {
+    fn default() -> Self {
+        Entity { id: 0, x: 0, y: 0 }
+    }
+}
+
 #[derive(Clone, Copy)]
 #[wasm_bindgen]
 pub struct Mover {
-    entity: Entity,
-    state: i32, // needs to be enum of state
-    velocity_x: i32,
-    velocity_y: i32,
-    orientation: f32,
-    target_x: i32,
-    target_y: i32,
-    energy: i32,
+    pub entity: Entity,
+    pub state: i32, // needs to be enum of state
+    pub velocity_x: i32,
+    pub velocity_y: i32,
+    pub orientation: f32,
+    pub target_x: i32,
+    pub target_y: i32,
+    pub energy: i32,
 }
 
 #[wasm_bindgen]
 impl Mover {
-    fn new(new_id: u32, new_x:i32, new_y: i32, new_state: i32, new_velocity_x: i32, new_velocity_y: i32, new_orientation: f32, new_target_x: i32,new_target_y: i32, new_energy: i32) -> Mover {
-        Mover { entity: Entity::new(new_id, new_x, new_y), state: new_state, velocity_x: new_velocity_x, velocity_y: new_velocity_y, orientation: new_orientation, target_x: new_target_x, target_y: new_target_y, energy: new_energy }
+    //fn new(new_id: u32, new_x:i32, new_y: i32, new_state: i32, new_velocity_x: i32, new_velocity_y: i32, new_orientation: f32, new_target_x: i32,new_target_y: i32, new_energy: i32) -> Mover {
+    //Mover { entity: Entity::default(), state: new_state, velocity_x: new_velocity_x, velocity_y: new_velocity_y, orientation: new_orientation, target_x: new_target_x, target_y: new_target_y, energy: new_energy }
+    //}
+    fn new(new_x: i32, new_y: i32, new_energy: i32) -> Mover {
+        Mover {
+            energy: new_energy,
+            entity: Entity {
+                x: new_x,
+                y: new_y,
+                ..Default::default()
+            },
+            ..Default::default()
+        }
     }
     fn get_state(&self) -> i32 {
         //change to enum in future
@@ -348,18 +381,37 @@ impl Mover {
     }
 }
 
+impl Default for Mover {
+    fn default() -> Self {
+        Mover {
+            entity: Entity::default(),
+            state: 0,
+            velocity_x: 0,
+            velocity_y: 0,
+            orientation: 0.0,
+            target_x: 0,
+            target_y: 0,
+            energy: 0,
+        }
+    }
+}
+
 #[derive(Clone, Copy)]
 #[wasm_bindgen]
 pub struct Rock {
-    entity: Entity,
-    diameter: u32,
-    height: u32,
+    pub entity: Entity,
+    pub diameter: u32,
+    pub height: u32,
 }
 
 #[wasm_bindgen]
 impl Rock {
-    fn new(new_id: u32, new_x: i32, new_y: i32, new_diameter: u32, new_height: u32) -> Rock {
-        Rock { entity: Entity::new(new_id, new_x, new_y), diameter: new_diameter, height: new_height }
+    fn new(new_x: i32, new_y: i32, new_diameter: u32, new_height: u32) -> Rock {
+        Rock {
+            entity: Entity {x: new_x, y: new_y, ..Default::default()},
+            diameter: new_diameter,
+            height: new_height,
+        }
     }
 
     pub fn get_x(&self) -> i32 {
@@ -382,20 +434,34 @@ impl Rock {
     }
 }
 
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, Default)]
 #[wasm_bindgen]
 pub struct Grazer {
-    mover: Mover,
+    pub mover: Mover,
     ticks_in_loc: i32, //minutes in cur location without moving max is 10 once at 10 need to move
 }
 
 #[wasm_bindgen]
 impl Grazer {
-    fn new(new_id: u32, new_x:i32, new_y: i32, new_state: i32, new_velocity_x: i32, new_velocity_y: i32, new_orientation: f32, new_target_x: i32,new_target_y: i32, new_energy: i32, new_min_in_loc: i32) -> Grazer {
-        Grazer { mover: Mover::new(new_id, new_x, new_y, new_state, new_velocity_x, new_velocity_y, new_orientation, new_target_x, new_target_y, new_energy), ticks_in_loc: new_min_in_loc }
+    fn new(new_x: i32, new_y: i32, new_energy: i32) -> Grazer {
+        Grazer {
+            mover: Mover {
+                entity: Entity {
+                    x: new_x,
+                    y: new_y,
+                    ..Default::default()
+                },
+                energy: new_energy,
+                ..Default::default()
+            },
+            ..Default::default()
+        }
     }
     pub fn get_ticks_in_loc(&self) -> i32 {
         self.ticks_in_loc
+    }
+    pub fn get_mover(&self) -> Mover {
+        return self.mover;
     }
     fn set_ticks_in_loc(&mut self, new_min_in_loc: i32) {
         self.ticks_in_loc = new_min_in_loc;
@@ -405,14 +471,18 @@ impl Grazer {
 #[derive(Clone, Copy)]
 #[wasm_bindgen]
 pub struct Plant {
-    entity: Entity,
-    diameter: u32,
+    pub entity: Entity,
+    pub diameter: u32,
 }
 
 #[wasm_bindgen]
 impl Plant {
+    // need getter for X and Y
     fn new(new_id: u32, new_x: i32, new_y: i32, new_diameter: u32) -> Plant {
-        Plant { entity: Entity::new(new_id, new_x, new_y), diameter: new_diameter}
+        Plant {
+            entity: Entity::new(new_id, new_x, new_y),
+            diameter: new_diameter,
+        }
     }
     pub fn get_diameter(&self) -> u32 {
         self.diameter
@@ -426,24 +496,28 @@ impl Plant {
     //need actual seeding functions
 }
 
-#[derive(Clone)]
+#[derive(Clone, Default)]
 #[wasm_bindgen]
 pub struct Predator {
-    mover: Mover,
+    pub mover: Mover,
     gen_seq: String,
-    family: Vec<i32>, //vector of family ids
-    time_family: f32, // time after mating that predator cares about family
-    is_pregnant: bool,
-    ticks_til_birth: u64,  // the first tick where the gestation period is over
-    mate_gen_seq: String, // mates gennetic sequence
+    family: Vec<i32>,     //vector of family ids
+    pub time_family: f32, // time after mating that predator cares about family
+    pub is_pregnant: bool,
+    pub ticks_til_birth: u64, // the first tick where the gestation period is over
+    mate_gen_seq: String,     // mates gennetic sequence
 }
 
 #[wasm_bindgen]
 impl Predator {
-    fn new(new_id: u32, new_x:i32, new_y: i32, new_state: i32, new_velocity_x: i32, new_velocity_y: i32, new_orientation: f32, new_target_x: i32,new_target_y: i32, new_energy: i32, new_min_in_loc: i32, new_gen_seq: String, new_family: Vec<i32>, new_time_family: f32, new_is_pregnant: bool, new_time_til_birth: u64, new_mate_gen_seq: String) -> Predator {
-        Predator { mover: Mover::new(new_id, new_x, new_y, new_state, new_velocity_x, new_velocity_y, new_orientation, new_target_x, new_target_y, new_energy), gen_seq: new_gen_seq, family: new_family, time_family: new_time_family, is_pregnant: new_is_pregnant, ticks_til_birth: new_time_til_birth, mate_gen_seq: new_mate_gen_seq }
+    fn new(new_x: i32, new_y: i32, new_energy: i32, new_gen_seq: String) -> Predator {
+        Predator {
+            mover: Mover::new(new_x, new_y, new_energy),
+            gen_seq: new_gen_seq,
+            ..Default::default()
+        }
     }
-    pub fn get_gen_seq(&self) ->  String {
+    pub fn get_gen_seq(&self) -> String {
         self.gen_seq.clone()
     }
     pub fn get_family(&self) -> Vec<i32> {
@@ -461,25 +535,25 @@ impl Predator {
     pub fn get_mate_seq(&self) -> String {
         self.mate_gen_seq.clone()
     }
-    fn set_gen_seq(&mut self, new_gen_seq: String) {
+    pub fn set_gen_seq(&mut self, new_gen_seq: String) {
         self.gen_seq = new_gen_seq;
     }
-    fn set_familiy(&mut self, new_family: Vec<i32>) {
+    pub fn set_familiy(&mut self, new_family: Vec<i32>) {
         self.family = new_family;
     }
-    fn add_family(&mut self, new_fam_id: i32) {
+    pub fn add_family(&mut self, new_fam_id: i32) {
         self.family.push(new_fam_id);
     }
-    fn set_time_family(&mut self, new_time_family: f32) {
+    pub fn set_time_family(&mut self, new_time_family: f32) {
         self.time_family = new_time_family;
     }
-    fn set_is_pregnant(&mut self, is_pregnant: bool) {
+    pub fn set_is_pregnant(&mut self, is_pregnant: bool) {
         self.is_pregnant = is_pregnant;
     }
-    fn set_ticks_til_birth(&mut self, map: Map, new_time_til_birth: u64) {
+    pub fn set_ticks_til_birth(&mut self, map: Map, new_time_til_birth: u64) {
         self.ticks_til_birth = new_time_til_birth + Map::get_current_tick(&map);
     }
-    fn set_mate_gen_seq(&mut self, new_mate_gen_seq: String) {
+    pub fn set_mate_gen_seq(&mut self, new_mate_gen_seq: String) {
         self.mate_gen_seq = new_mate_gen_seq;
     }
 }
--- www/index.js
+++ www/index.js
@@ -1,4 +1,4 @@
-import {Map} from "life_simulation";
+import {Map, Predator} from "life_simulation";
 
 var canvas = document.getElementById("game-canvas");
 var ctx = canvas.getContext("2d");
@@ -14,37 +14,94 @@ const render = () => {
 
 const map = Map.new();
 
-var r = map.add_rock(1,10,15,7,9);
-map.add_rock(2,10,15,7,8);
-
-map.set_width(10);
-map.set_height(10);
-
-for(var rock of map.get_rocks()) {
-    console.log(rock.get_height());
-}
 
 render()
 
 var fileInputElement = document.getElementById("file-input");
 fileInputElement.addEventListener("change", e => fileInputElement.files[0].text().then((xmlText) => {
-    console.log(xmlText);
     var xmlText = xmlText.replace(/\s/g,"");
     var parser = new DOMParser();
     var xmlDoc = parser.parseFromString(xmlText,"text/xml");
     var simulation = xmlDoc.getRootNode().childNodes[0];
     var landBounds = simulation.childNodes[0];
     var plants = simulation.childNodes[1];
-    //plants.getElementsByTagName("INITIAL_PLANT_COUNT")[0].childNodes[0].nodeValue
-    //always returns a string
     var grazers = simulation.childNodes[2];
     var predators = simulation.childNodes[3];
     var obstacles = simulation.childNodes[4];
     
+    // map inputs
+    map.set_width(parseFloat(landBounds.getElementsByTagName("WIDTH")[0].childNodes[0].nodeValue));
+    map.set_height(parseFloat(landBounds.getElementsByTagName("HEIGHT")[0].childNodes[0].nodeValue));
+
+    // plant metadata
+    map.set_init_plant_count(parseInt(plants.getElementsByTagName("INITIAL_PLANT_COUNT")[0].childNodes[0].nodeValue));
+    map.set_growth_rate(parseFloat(plants.getElementsByTagName("GROWTH_RATE")[0].childNodes[0].nodeValue));
+    map.set_max_size(parseInt(plants.getElementsByTagName("MAX_SIZE")[0].childNodes[0].nodeValue));
+    map.set_max_seed_cast_distance(parseInt(plants.getElementsByTagName("MAX_SEED_CAST_DISTANCE")[0].childNodes[0].nodeValue));
+    map.set_max_seed_number(parseInt(plants.getElementsByTagName("MAX_SEED_NUMBER")[0].childNodes[0].nodeValue));
+    map.set_max_size(parseFloat(plants.getElementsByTagName("MAX_SIZE")[0].childNodes[0].nodeValue));
+    map.set_seed_viability(parseFloat(plants.getElementsByTagName("SEED_VIABILITY")[0].childNodes[0].nodeValue));
+    
     plants = plants.getElementsByTagName("PLANT");
-    console.log(plants);
     for (var plant of plants) {
-        console.log(plant);
+        var plantX = parseInt(plant.getElementsByTagName("X_POS")[0].childNodes[0].nodeValue);
+        var plantY = parseInt(plant.getElementsByTagName("Y_POS")[0].childNodes[0].nodeValue);
+        var plantD = parseInt(plant.getElementsByTagName("P_DIAMETER")[0].childNodes[0].nodeValue);
+        map.add_plant(1, plantX, plantY, plantD)
     }
+
+    // grazer metadata
+    map.set_init_grazer_count(parseInt(plants.getElementsByTagName("INITIAL_GRAZER_COUNT")[0].childNodes[0].nodeValue));
+    map.set_grazer_energy_input(parseInt(grazers.getElementsByTagName("G_ENERGY_INPUT")[0].childNodes[0].nodeValue));
+    map.set_grazer_energy_output(parseInt(grazers.getElementsByTagName("G_ENERGY_OUTPUT")[0].childNodes[0].nodeValue));
+    map.set_grazer_energy_to_reproduce(parseInt(grazers.getElementsByTagName("G_ENERGY_TO_REPRODUCE")[0].childNodes[0].nodeValue));
+    map.set_grazer_maintain_speed(parseFloat(grazers.getElementsByTagName("G_MAINTAIN_SPEED")[0].childNodes[0].nodeValue));
+    map.set_grazer_max_speed(parseFloat(grazers.getElementsByTagName("G_MAX_SPEED")[0].childNodes[0].nodeValue));
+
+    grazers = grazers.getElementsByTagName("GRAZER");
+    for (var grazer of grazers) {
+        var grazerX = parseInt(grazer.getElementsByTagName("X_POS")[0].childNodes[0].nodeValue);
+        var grazerY = parseInt(grazer.getElementsByTagName("Y_POS")[0].childNodes[0].nodeValue);
+        var grazerEnergy = parseInt(grazer.getElementsByTagName("G_ENERGY_LEVEL")[0].childNodes[0].nodeValue);
+        map.add_grazer(grazerX, grazerY, grazerEnergy)
+    }
+
+    // predator metadata
+    map.set_init_predator_count(parseInt(plants.getElementsByTagName("INITIAL_PREDATOR_COUNT")[0].childNodes[0].nodeValue));
+    map.set_max_speed_hod(parseFloat(predators.getElementsByTagName("MAX_SPEED_HOD")[0].childNodes[0].nodeValue));
+    map.set_max_speed_hed(parseFloat(predators.getElementsByTagName("MAX_SPEED_HED")[0].childNodes[0].nodeValue));
+    map.set_max_speed_hor(parseFloat(predators.getElementsByTagName("MAX_SPEED_HOR")[0].childNodes[0].nodeValue));
+    map.set_predator_maintain_speed(parseFloat(predators.getElementsByTagName("P_MAINTAIN_SPEED")[0].childNodes[0].nodeValue));
+    map.set_predator_energy_output(parseInt(predators.getElementsByTagName("P_ENERGY_OUTPUT")[0].childNodes[0].nodeValue));
+    map.set_predator_energy_to_reproduce(parseInt(predators.getElementsByTagName("P_ENERGY_TO_REPRODUCE")[0].childNodes[0].nodeValue));
+    map.set_predator_max_offspring(parseInt(predators.getElementsByTagName("P_MAX_OFFSPRING")[0].childNodes[0].nodeValue));
+    map.set_predator_gestation(parseFloat(predators.getElementsByTagName("P_GESTATION")[0].childNodes[0].nodeValue));
+    map.set_predator_offspring_energy(parseInt(predators.getElementsByTagName("P_OFFSPRING_ENERGY")[0].childNodes[0].nodeValue));
+    
+    predators = predators.getElementsByTagName("PREDATOR");
+    for (var predator of predators) {
+        var predatorX = parseInt(predator.getElementsByTagName("X_POS")[0].childNodes[0].nodeValue);
+        var predatorY = parseInt(predator.getElementsByTagName("Y_POS")[0].childNodes[0].nodeValue);
+        var predatorEnergy = parseInt(predator.getElementsByTagName("P_ENERGY_LEVEL")[0].childNodes[0].nodeValue);
+        var genes = predator.getElementsByTagName("GENOTYPE")[0].childNodes[0].nodeValue;
+        map.add_predator(predatorX, predatorY, predatorEnergy, genes);
+    }    
+
+    var rocks = obstacles.getElementsByTagName("OBSTACLE");
+    for (var rock of rocks) {
+        var rockX = parseInt(rock.getElementsByTagName("X_POS")[0].childNodes[0].nodeValue);
+        var rockY = parseInt(rock.getElementsByTagName("Y_POS")[0].childNodes[0].nodeValue);
+        var rockD = parseInt(rock.getElementsByTagName("O_DIAMETER")[0].childNodes[0].nodeValue);
+        var rockH = parseInt(rock.getElementsByTagName("O_HEIGHT")[0].childNodes[0].nodeValue);
+        map.add_rock(rockX, rockY, rockD, rockH);
+    }
+
+    console.log(map)
+    console.log(map.get_grazers())
+    console.log(map.get_rocks())
+    console.log(map.get_predators())
+    console.log(map.get_plants())
+
+    
     
 }));
